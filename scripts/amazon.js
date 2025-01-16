4d57fa81d98b91330f86aa257c29bca23d80c595
Benjamin <benjamin.4179@gmail.com>
2025-01-16 12:43:30 -0800

    Used products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,5 @@
 console.log("hello");
 
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    //remeber javascript has problems dealing with decibals change int cents
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    //remeber javascript has problems dealing with decibals change int cents
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    //remeber javascript has problems dealing with decibals change int cents
-    priceCents: 799,
-  },
-];
-
 let productHTML = "";
 
 products.forEach((products) => {
